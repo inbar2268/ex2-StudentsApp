cd6f4b843492c58c818db1145f36c90785fed41a
inbar2268 <inbar2268@gmail.com>
2025-01-09 08:23:41 +0200

    add edit button

--- app/src/main/java/com/example/studentsapp/StudentDetails.kt
+++ app/src/main/java/com/example/studentsapp/StudentDetails.kt
@@ -1,6 +1,9 @@
 package com.example.studentsapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.IntentCompat.getSerializableExtra
@@ -15,6 +18,7 @@ class StudentDetails : AppCompatActivity() {
     private lateinit var binding: ActivityStudentDetailsBinding
     private var student: Student? = null
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStudentDetailsBinding.inflate(layoutInflater)
@@ -23,14 +27,17 @@ class StudentDetails : AppCompatActivity() {
         student = intent.getSerializableExtra("student_data") as Student?
 
         student?.let {
-            binding.studentIdTextView.text = it.id
-            binding.studentNameTextView.text = it.name
-            binding.studentAddressTextView.text = it.address
-            binding.studentPhoneTextView.text = it.phone
+            binding.studentIdTextView.text = "Id: ${it.id}"
+            binding.studentNameTextView.text = "Name: ${it.name}"
+            binding.studentAddressTextView.text = "Address: ${it.address}"
+            binding.studentPhoneTextView.text = "Phone: ${it.phone}"
             binding.studentIsChecked.isChecked = it.isChecked
             binding.studentIsChecked.isClickable = false
         }
 
-
+        binding.editStudentButton.setOnClickListener{
+            //val intent = Intent(this, EditStudent::class.java)
+            //startActivity(intent)
+        }
     }
 }
